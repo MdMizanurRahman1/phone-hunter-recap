7d9255a1e1872d372a65d5cd22dec7532f513872
MdMizanurRahman1 <mizan.rahman2217@gmail.com>
2023-03-01 12:49:40 +0200

    spinner done

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
-
+//loading phones
 const loadPhones = async (searchText) => {
     const url = `https://openapi.programming-hero.com/api/phones?search=${searchText}`;
     const res = await fetch(url);
     const data = await res.json();
     displayPhones(data.data);
 }
-
+//display phones
 const displayPhones = phones => {
     const phonesContainer = document.getElementById('phones-container');
 
@@ -29,6 +29,7 @@ const displayPhones = phones => {
     //display all phones
     phones.forEach(phone => {
         // console.log(phone)
+        //create child elements 
         const phoneDiv = document.createElement('div');
         phoneDiv.classList.add('col');
         phoneDiv.innerHTML = `
@@ -45,17 +46,36 @@ const displayPhones = phones => {
 
         phonesContainer.appendChild(phoneDiv);
     })
-
+    //stop loader or spinner
+    toggleSpinner(false);
 }
 
 //search phone
 document.getElementById('search-btn').addEventListener('click', function () {
+
+    //start spinner or loader
+    toggleSpinner(true);
+
     const searchField = document.getElementById('input-field');
     const searchText = searchField.value;
     loadPhones(searchText);
 })
 
 
+//function for start loader or spinner
 
-//loadPhones();
+const toggleSpinner = isLoading => {
+    const loader = document.getElementById('loader');
+
+    if (isLoading) {
+        loader.classList.remove('d-none');
+    }
+    else {
+        loader.classList.add('d-none');
+    }
+}
+
+
+
+//loadPhones('iphone');
 
